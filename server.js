04d5eafb9ad6bf466eb7f390a6d023306fd5ae64
Suraj Thapa <stsurajst@gmail.com>
2020-09-06 10:19:29 +0545

    routes added in server file

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 // Define Routes
+
+app.use('api/users', require('./routes/api/user'));
+app.use('api/auth', require('./routes/api/auth'));
 app.use('api/stripe/createAccount', require('./routes/api/stripe'));
 
 const PORT = process.env.PORT || 5000;
